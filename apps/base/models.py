4bd74cbce8ff20a3192663a3b3052b946513081e
Christos Marroquin <christos.marroquin@hotmail.com>
2023-07-16 23:54:24 -0600

    modelo base

---
+++ apps/base/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Base(models.Model):
+    id = models.AutoField(primary_key=True)
+    state = models.BooleanField("Estado",default=True)
+    created_at = models.DateField("Fecha de creación", auto_now_add=True)
+    modified_at = models.DateField("Fecha de modificación", auto_now=True)
+    deleted_at = models.DateField("Fecha de eliminación")
+
+    class Meta:
+        abstract = True
+        verbose_name = "Modelo base"
+        verbose_name_plural = "Modelos base"
--- dapi/settings.py
+++ dapi/settings.py
@@ -40,7 +40,7 @@ BASE_APPS = [
     "django.contrib.staticfiles",
 ]
 
-LOCAL_APPS = ["apps.users"]
+LOCAL_APPS = ["apps.users", "apps.base"]
 
 THIRD_APPS = [
     "rest_framework",
