4d55238730d7c0f439430b5201b7f082a87a25ba
Christos Marroquin <christos.marroquin@gmail.com>
2023-09-29 00:25:34 -0600

    1 minuto

--- dapi/settings.py
+++ dapi/settings.py
@@ -183,7 +183,7 @@ REST_FRAMEWORK = {
 
 # SIMPLE JWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
